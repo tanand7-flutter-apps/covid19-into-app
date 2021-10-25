52f41fcc079809c8adba466bfa36a957f63ae0fa
Anand T <athankappan@thoughtlinetech.com>
2021-10-25 11:58:33 +0530

    Project Created

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CovidInfoApp());
+}
+
+class CovidInfoApp extends StatelessWidget {
+  const CovidInfoApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
